958445380dbcef7045ac0a322f678e2ee2b5872b
jayeshkolpurath <67106845+jayeshkolpurath@users.noreply.github.com>
2020-07-15 17:05:27 +0530

    Add files via upload

---
+++ School Admin/School Admin/Helpers/Login/LoginHelper.js
@@ -0,0 +1,16 @@
+﻿class LoginHelper {
+    constructor(User) {
+        this.User = User;
+    }
+    FetchUserWithCredentials(UserName, Password) {
+        /*
+        Check if User exists in database with the given username and password
+        if yes return true and return false otherwise.
+        */
+        var objUserModel = null;
+        if (UserName == "test" && Password == "test") {
+            objUserModel = new UserModel(UserName, Password); //dummy code
+        }
+        return objUserModel; //return dummy value - to be replaced with actual code
+    }
+}
